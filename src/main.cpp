b905034d65980980f32a4c34ebf8dfb26ebbe002
I'm Pancake <slavik15971597@gmail.com>
2023-10-16 20:28:20 +0300

    Refactor source tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
-/** Made by I'Pancake **/
+/** Made by I'm Pancake **/
 
-#include "choose_your_fate.h"
+#include "choose_your_fate/choose_your_fate.h"
 #include <exception>
 #include <iostream>
 
 int main(int argc, char **argv)
 {
-    try {
+    try
+    {
         choose_your_fate(argc, argv);
-
-    } catch (std::exception &e) {
+    }
+    catch (std::exception &e)
+    {
         std::cerr << e.what() << std::endl;
     }
 }
